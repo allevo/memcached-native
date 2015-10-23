b1c88e5faa6aface5f1869455daf547ea7922bf2
Tommaso Allevi <tomallevi@gmail.com>
2015-10-23 18:34:49 +0200

    Try #4

--- test/benchmark.js
+++ test/benchmark.js
@@ -10,7 +10,7 @@ var PORT = helper.getMemcachePort();
 var HOST = helper.getMemcacheHost();
 
 var memcachedClient;
-describe('benchmark', function () {
+xdescribe('benchmark', function () {
   beforeEach(function() {
     memcachedClient = new Client('--SERVER=' + HOST + ':' + PORT + ' --SUPPORT-CAS');
     memcachedClient.start();
